7fb4e1041a46a952e1d7c5a6027bd697b1988fc6
Iskender Piyale-Sheard <ipiyale@gmail.com>
2017-02-20 04:44:07 -0500

    Newsletter trial

--- assets/js/ajaxchimp-config.js
+++ assets/js/ajaxchimp-config.js
@@ -4,7 +4,7 @@ $( document ).ready(function() {
     $('.mailchimp').ajaxChimp({
         callback: mailchimpCallback,
         //replace bellow url with your own mailchimp form post url inside the url: "---".
-        url: "http://trendytheme.us9.list-manage.com/subscribe/post?u=85ba3666ffb519396fbe64dc5&amp;id=c335e5ec53" 
+        url: "http://sillyskinny.us3.list-manage.com/subscribe?u=9323f9d9f0b350a8ec35fb576&id=eebf882952" 
     }); 
     function mailchimpCallback(resp) {
          if (resp.result === 'success') {
